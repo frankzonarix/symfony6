70c180dc944197adcc55bd98e146f8e96347eedf
Frank <frank@zonarix.net>
2022-08-24 16:52:39 -0500

    Rendering template through twig service instead of ->render

--- src/Controller/VinylController/VinylController.php
+++ src/Controller/VinylController/VinylController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\U;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twig\Environment;
 
 /**
  * Vinyl Controller
@@ -24,7 +25,7 @@ class VinylController extends AbstractController
      * @return Response
      **/
     #[Route('/', name: 'app_homepage')]
-    public function homepage(): Response
+    public function homepage(Environment $twig): Response
     {
 
         /* Making it multidimensional */
@@ -36,13 +37,15 @@ class VinylController extends AbstractController
             ['artist' => 'Morgana Lefay', 'song' => 'The Boon he gives']
         ];
 
-
-        return $this->render(
-            'vinyl/homepage.html.twig', [
+        $html = $twig->render('vinyl/homepage.html.twig', [
+        //return $this->render( //** This only works within a controller, if you are in a
+            class where you cannot access $this>render you will have to do it through the twig service */
+            'vinyl/homepage.html.twig',
             'title' => 'PB & Jams',
             'tracks' => $tracks,
-            ]
-        );
+        ]);
+        //dd($html); shows the whole rendered template
+        return new Response($html);
     }
 
     /**
