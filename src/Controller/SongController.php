3db68f3228a4eb1a2a37ca5f6108892d67d285df
Frank <frank@zonarix.net>
2022-08-24 16:52:02 -0500

    adding logger and comment for how to autowire

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -12,6 +12,9 @@ class SongController extends AbstractController {
 //Note argument without '' in curly braces
     #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
     //unlike in the tutorial the typehint has to match JsonResponse instead of Response
+
+    //php bin/console debug:autowiring log
+    // shows: Psr\Log\LoggerInterface (monolog.logger)
     public function getSong(int $id, LoggerInterface $logger): JsonResponse {
         //dd($id);
         //Example song data
@@ -22,7 +25,7 @@ class SongController extends AbstractController {
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
 
-        $logger->info('Retruning API response for song {song}', [
+        $logger->info('Returning API response for song {song}', [
             'song' => $id,
         ]);
         // https://chrome.google.com/webstore/detail/json-formatter/bcjindcccaagfpapjjmafapmmgkkhgoa/related?hl=es
