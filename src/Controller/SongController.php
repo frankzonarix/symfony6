d02be2d846692bd0a9f2ba3a28150a1fa04c23af
Frank <frank@zonarix.net>
2022-08-23 19:27:52 -0500

    created the first JSonResponse

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -4,12 +4,24 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SongController extends AbstractController {
 //Note argument without '' in curly braces
     #[Route('/api/songs/{id}')]
-    public function getSong($id): Response {
-        dd($id);
+    //unlike in the tutorial the typehint has to match JsonResponse instead of Response
+    public function getSong($id): JsonResponse {
+        //dd($id);
+        //Example song data
+        // TODO should come from database
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+
+
+        return new JsonResponse($song);
     }
 
 
